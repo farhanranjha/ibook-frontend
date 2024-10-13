1ce142c1be9e2f25a0777d416f332fdac3fb8df8
Farhan <farhaan.ranjha@gmail.com>
2024-10-14 00:31:00 +0500

    added services for projects

--- src/app/project/services/project.service.ts
+++ src/app/project/services/project.service.ts
@@ -1,9 +1,33 @@
-import { Injectable } from '@angular/core';
+import { Injectable } from "@angular/core";
+import { HttpClient, HttpHeaders } from "@angular/common/http";
+import { Observable } from "rxjs";
+import { environment } from "../../../environments/environment.development";
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: "root",
 })
 export class ProjectService {
+  private baseUrl = environment.BASE_URL;
 
-  constructor() { }
+  constructor(private http: HttpClient) {}
+
+  createProject(title: string, description: string): Observable<any> {
+    const url = `${this.baseUrl}/projects/`;
+
+    const headers = new HttpHeaders({
+      "Content-Type": "application/json",
+    });
+
+    const body = {
+      title,
+      description,
+    };
+
+    return this.http.post(url, body, { headers });
+  }
+
+  getProjects(token: string): Observable<any> {
+    const url = `${this.baseUrl}/projects/`;
+    return this.http.get(url);
+  }
 }
