8662735776e1b33b59e55dfd6a602d23c8c4233c
Farhan <farhaan.ranjha@gmail.com>
2024-10-14 00:09:55 +0500

    added interceptor to append token in req

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,23 @@
 import { LocationStrategy, PathLocationStrategy } from "@angular/common";
-import { HttpClientModule } from "@angular/common/http";
+import { HTTP_INTERCEPTORS, HttpClientModule } from "@angular/common/http";
 import { NgModule } from "@angular/core";
 import { AppRoutingModule } from "./app-routing.module";
 import { AppComponent } from "./app.component";
 import { BrowserModule } from "@angular/platform-browser";
 import { StoreModule } from "@ngrx/store";
 import { userReducer } from "./store/user.reducer";
+import { AuthInterceptor } from "./interceptors/auth.interceptors";
 
 @NgModule({
   declarations: [AppComponent],
   imports: [HttpClientModule, AppRoutingModule, BrowserModule, StoreModule.forRoot({ user: userReducer })],
-  providers: [{ provide: LocationStrategy, useClass: PathLocationStrategy }],
+  providers: [
+    {
+      provide: HTTP_INTERCEPTORS,
+      useClass: AuthInterceptor,
+      multi: true,
+    },
+  ],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
