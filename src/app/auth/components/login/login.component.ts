f14ca2afddf8c9a62dc6ad1c9801da9b0ec50eb4
Farhan <farhaan.ranjha@gmail.com>
2024-10-14 01:16:24 +0500

    made login and made it functional

--- src/app/auth/components/login/login.component.ts
+++ src/app/auth/components/login/login.component.ts
@@ -1,12 +1,43 @@
-import { Component } from '@angular/core';
+import { CommonModule } from "@angular/common";
+import { Component } from "@angular/core";
+import { FormsModule } from "@angular/forms";
+import { ButtonModule } from "primeng/button";
+import { FloatLabelModule } from "primeng/floatlabel";
+import { InputTextModule } from "primeng/inputtext";
+import { PasswordModule } from "primeng/password";
+import { AuthService } from "../../services/auth.service";
+import { User } from "../../../store/user.model";
+import { Store } from "@ngrx/store";
+import { add } from "../../../store/user.actions";
+import { Router } from "@angular/router";
 
 @Component({
-  selector: 'app-login',
+  selector: "app-login",
   standalone: true,
-  imports: [],
-  templateUrl: './login.component.html',
-  styleUrl: './login.component.scss'
+  imports: [CommonModule, ButtonModule, FormsModule, InputTextModule, FloatLabelModule, PasswordModule],
+  templateUrl: "./login.component.html",
+  styleUrls: ["./login.component.scss"],
 })
 export class LoginComponent {
+  username: string = "";
+  password: string = "";
 
+  constructor(private authService: AuthService, private store: Store, private router: Router) {}
+
+  submitLogin() {
+    this.authService.login(this.username, this.password).subscribe({
+      next: (response) => {
+        const user: User = {
+          username: response.email,
+          accessToken: response.access,
+          refreshToken: response.refresh,
+        };
+        this.store.dispatch(add({ user }));
+        this.router.navigate(["/"]);
+      },
+      error: (error) => {
+        console.error("Login failed", error);
+      },
+    });
+  }
 }
