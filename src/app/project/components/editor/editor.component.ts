d5b8add40888288560a218059123825993082600
Farhan <farhaan.ranjha@gmail.com>
2024-10-14 13:06:43 +0500

    created editor page with primeng editor

--- src/app/project/components/editor/editor.component.ts
+++ src/app/project/components/editor/editor.component.ts
@@ -1,12 +1,37 @@
-import { Component } from '@angular/core';
+import { Component } from "@angular/core";
+import { FormsModule } from "@angular/forms";
+import { ButtonModule } from "primeng/button";
+import { EditorModule } from "primeng/editor";
+import { InputTextModule } from "primeng/inputtext";
+import { ProjectService } from "../../services/project.service";
+import { MessageService } from "primeng/api";
+import { ToastModule } from "primeng/toast";
+import { Router } from "@angular/router";
 
 @Component({
-  selector: 'app-editor',
+  selector: "app-editor",
   standalone: true,
-  imports: [],
-  templateUrl: './editor.component.html',
-  styleUrl: './editor.component.scss'
+  imports: [EditorModule, FormsModule, ButtonModule, InputTextModule, ToastModule],
+  templateUrl: "./editor.component.html",
+  styleUrl: "./editor.component.scss",
+  providers: [MessageService],
 })
 export class EditorComponent {
+  constructor(private router: Router, private projectService: ProjectService, private messageService: MessageService) {}
 
+  text: string = "";
+  title: string = "";
+  createDocument() {
+    this.projectService.createProject(this.title, this.text).subscribe({
+      next: () => {
+        this.messageService.add({ severity: "success", summary: "Success", detail: "Document saved!" });
+      },
+      error: (err) => {
+        this.messageService.add({ severity: "error", summary: "Error", detail: "Failed to save document!" });
+      },
+    });
+  }
+  goToDashboard() {
+    this.router.navigateByUrl("/dashboard");
+  }
 }
